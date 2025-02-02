d4ea756360014d76e91ec490320cc1c5d947288a
Tarmo <liivamagi.tarmo@gmail.com>
2025-02-02 17:07:11 +0200

    react routing

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { Box, Heading, Button, VStack, Text } from "@chakra-ui/react";
+
+const HomePage = () => {
+    return (
+      <Box p={8} textAlign="center">
+        <VStack spacing={4}>
+          <Heading size="2xl">Phishing Awareness Training Platform</Heading>
+          <Text fontSize="lg" color="gray.600">
+            Learn how to recognize and avoid phishing attacks through interactive training.
+          </Text>
+          <VStack spacing={4} mt={8}>
+            <Link to="/quiz" colorScheme="teal" size="lg">
+                 Start Training
+            </Link>
+            <Link as={Link} to="/tutorial" colorScheme="blue" size="lg">
+              Tutorial
+            </Link>
+          </VStack>
+        </VStack>
+      </Box>
+    );
+  };
+
+  export default HomePage;
+  
---
+++ client/src/pages/QuizPage.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading, Button, VStack, Text } from "@chakra-ui/react";
+
+const QuizPage = () => (
+    <Box p={8} textAlign="center">
+      <Heading>Quiz Page</Heading>
+      <Text mt={4}>Test your skills in recognizing phishing attempts!</Text>
+    </Box>
+  );
+
+  export default QuizPage;
