fa3e8acf72db584c1f5a683b8ad99617fdc6af88
TLiiv <110547191+TLiiv@users.noreply.github.com>
2025-04-12 12:38:57 +0300

    close button to instruction modal

--- client/src/pages/TestPage.tsx
+++ client/src/pages/TestPage.tsx
@@ -14,12 +14,16 @@ import {
   Theme,
   VStack,
   Input,
+  CloseButton,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const TestPage: React.FC = () => {
   const [username, setUsername] = useState("");
   const [testStarted, setTestStarted] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleStartTest = () => {
     if (username.trim()) {
@@ -27,71 +31,81 @@ const TestPage: React.FC = () => {
     }
   };
 
+  const handleClose = () => {
+    navigate("/");
+    // modal if closed twice wont let click on links anymore so we need to refresh the page...
+    navigate(0);
+  };
+
   return (
     <Box maxW="1680px" mx="auto" px={4} position="relative">
       <Theme appearance="light" borderRadius="4xl">
-        {!testStarted && (
-          <Dialog.Root
-            open
-            modal
-            preventScroll
-            closeOnEscape={false}
-            closeOnInteractOutside={false}
-          >
-            <Portal>
-              <DialogBackdrop />
-              <DialogPositioner>
-                <DialogContent borderRadius="2xl" bg="white" maxW="lg" p={6}>
-                  <DialogHeader>
-                    <DialogTitle
-                      fontSize="2xl"
-                      fontWeight="bold"
-                      color="gray.800"
+        <Dialog.Root
+          open={!testStarted}
+          unmountOnExit={true}
+          modal
+          preventScroll
+          closeOnEscape={false}
+          closeOnInteractOutside={false}
+        >
+          <Portal>
+            <DialogBackdrop />
+            <DialogPositioner>
+              <DialogContent borderRadius="2xl" bg="white" maxW="lg" p={6}>
+                <DialogHeader>
+                  <DialogTitle
+                    fontSize="2xl"
+                    fontWeight="bold"
+                    color="gray.800"
+                  >
+                    Tere tulemast andmepüügi emaili tuvastamise testi!
+                  </DialogTitle>
+                </DialogHeader>
+                <DialogBody color="gray.800">
+                  <VStack align="stretch" fontWeight="bold" p={4}>
+                    <Text mt={2}>
+                      Selles testis saad proovida, kui hästi suudad eristada
+                      õigeid e-kirju valedest. Testis on kokku 8 taset.
+                    </Text>
+                    <Text mt={2}>
+                      Sina pead ära arvama, kas tegemist on andmepüügi või õige
+                      e-kirjaga. Alates viiendast tasemest muutuvad osad lingid
+                      aktiivseks, kuna meili sisu põhjal ei ole nii lihtne aru
+                      saada, kas tegemist on andmepüügiga või mitte.
+                    </Text>
+                    <Text mt={2}>
+                      Testi oma teadlikkust ja uuri välja, kas sa suudad
+                      andmepüügi eristada õigest e-kirjast. Edu!
+                    </Text>
+                    <Text mt={2}>
+                      Enne alustamist sisesta oma nimi ja vajuta „Alusta testi”.
+                    </Text>
+                    <Input
+                      placeholder="Sisesta oma nimi"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <Button
+                      background="teal.500"
+                      color="gray.100"
+                      onClick={handleStartTest}
+                      disabled={!username.trim()}
                     >
-                      Tere tulemast andmepüügi emaili tuvastamise testi!
-                    </DialogTitle>
-                  </DialogHeader>
-                  <DialogBody color="gray.800">
-                    <VStack align="stretch" fontWeight="bold" p={4}>
-                      <Text mt={2}>
-                        Selles testis saad proovida, kui hästi suudad eristada
-                        õigeid e-kirju valedest. Testis on kokku 8 taset.
-                      </Text>
-                      <Text mt={2}>
-                        Sina pead ära arvama, kas tegemist on andmepüügi või
-                        õige e-kirjaga. Alates viiendast tasemest muutuvad osad
-                        lingid aktiivseks, kuna meili sisu põhjal ei ole nii
-                        lihtne aru saada, kas tegemist on andmepüügiga või
-                        mitte.
-                      </Text>
-                      <Text mt={2}>
-                        Testi oma teadlikkust ja uuri välja, kas sa suudad
-                        andmepüügi eristada õigest e-kirjast. Edu!
-                      </Text>
-                      <Text mt={2}>
-                        Enne alustamist sisesta oma nimi ja vajuta „Alusta
-                        testi”.
-                      </Text>
-                      <Input
-                        placeholder="Sisesta oma nimi"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                      />
-                      <Button
-                        background="teal.500"
-                        color="gray.100"
-                        onClick={handleStartTest}
-                        disabled={!username.trim()}
-                      >
-                        Alusta testi
-                      </Button>
-                    </VStack>
-                  </DialogBody>
-                </DialogContent>
-              </DialogPositioner>
-            </Portal>
-          </Dialog.Root>
-        )}
+                      Alusta testi
+                    </Button>
+                  </VStack>
+                </DialogBody>
+                <Dialog.CloseTrigger asChild>
+                  <CloseButton
+                    size="md"
+                    onClick={handleClose}
+                    variant="solid"
+                  />
+                </Dialog.CloseTrigger>
+              </DialogContent>
+            </DialogPositioner>
+          </Portal>
+        </Dialog.Root>
 
         {testStarted && <EmailDashboard username={username} />}
       </Theme>
